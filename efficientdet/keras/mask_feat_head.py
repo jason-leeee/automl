94335cf86d4806847a682f4e503bc73d76158e14
ml6 <jason.li@ml6.eu>
2021-01-12 17:39:06 +0100

    mask feat forward pass

--- efficientdet/keras/mask_feat_head.py
+++ efficientdet/keras/mask_feat_head.py
@@ -1,3 +1,6 @@
+import tensorflow as tf
+import numpy as np
+
 class MaskFeatHead(tf.keras.layers.Layer):
 
     def __init__(self,
@@ -13,7 +16,6 @@ class MaskFeatHead(tf.keras.layers.Layer):
                 name="mask_feat_head"):
         super().__init__(name=name)
         self.in_channels = in_channels
-        self.level_sizes = level_sizes
         self.out_channels = out_channels
         self.start_level = start_level
         self.end_level = end_level
@@ -26,38 +28,18 @@ class MaskFeatHead(tf.keras.layers.Layer):
         for i in range(start_level, end_level + 1):
             level_convs = tf.keras.Sequential()
             if i == 0:
-                input_shape = (batch_size, level_sizes[i][0], level_sizes[i][1], self.in_channels)
+                
                 level_convs.add(
                     tf.keras.layers.Conv2D(
                         self.out_channels,
                         3,
-                        padding='same',
-                        input_shape=input_shape
+                        padding='same'
                     )
                 )
                 self.all_level_convs.append(level_convs)
                 continue
 
-            for j in range(i):
-                if j == 0:
-                    chn = self.in_channels+2 if i==3 else self.in_channels
-                    input_shape = (batch_size, self.level_sizes[j][0], self.level_sizes[j][1], chn)
-                    level_convs.add(
-                        tf.keras.layers.Conv2D(
-                            self.out_channels,
-                            3,
-                            padding='same',
-                            input_shape=input_shape
-                        )
-                    )
-                    level_convs.add(
-                        tf.keras.layers.UpSampling2D(
-                            size=(2, 2),
-                            interpolation='bilinear'
-                        )
-                    )
-                    continue
-
+            for j in range(i-2):
                 level_convs.add(
                     tf.keras.layers.Conv2D(
                         self.out_channels,
@@ -77,23 +59,28 @@ class MaskFeatHead(tf.keras.layers.Layer):
 
     def call(self, inputs):
         assert len(inputs) == (self.end_level - self.start_level + 1)
+        #for input in inputs:
+        #    print("fpn feature map size:", input.shape)
 
         feature_add_all_level = self.all_level_convs[0](inputs[0])
+
         for i in range(1, len(inputs)):
             input_p = inputs[i]
             if i == 3:
                 input_feat = input_p
-                x_range = tf.linspace(-1, 1, input_feat.shape[-2])
-                y_range = tf.linspace(-1, 1, input_feat.shape[-3])
-                y, x = tf.meshgrid(y_range, x_range)
+                x_range = tf.linspace(-1, 1, input_feat.shape[-3])
+                y_range = tf.linspace(-1, 1, input_feat.shape[-2])
+                y, x = tf.meshgrid(y_range, x_range)   
                 y = tf.broadcast_to(y, [input_feat.shape[0], -1, -1, 1])
-                x = tf.broadcast_to(x, [input_feat.shape[0], 1, -1, -1])
+                x = tf.broadcast_to(x, [input_feat.shape[0], -1, -1, 1])
                 coord_feat = tf.concat([x, y], -1)
                 input_p = tf.concat([input_p, coord_feat], -1)
                 
             feature_add_all_level += self.all_level_convs[i](input_p)
-
+        #for layer in self.all_level_convs:
+        #    layer.summary()
         feature_pred = self.conv_pred(feature_add_all_level)
+        #print("Final feature map shape:", feature_pred.shape)
         return feature_pred
 
 
